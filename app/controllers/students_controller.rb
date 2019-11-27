e0c2438386e3bc89d5a20dbd2671d03708b59c3f
Annie Truex <artruex@gmail.com>
2019-11-27 02:33:02 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,7 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
+    #@student = Student.find(params[:id])
   end
   
   def new 
@@ -16,10 +16,18 @@ class StudentsController < ApplicationController
     redirect_to new_student_path
   end
 
+<<<<<<< HEAD
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+=======
+  # private
+
+  #   def set_student
+  #     @student = Student.find(params[:id])
+  #   end
+>>>>>>> c1211ba830c5c68926738fafcee5cb3db18a3a62
 
 end
